c2a6a90b7940a7e52edb2479fa1371ffe9b1ab12
krishna2300320120132 <krishnafamily32@gmail.com>
2026-07-07 23:22:05 +0530

    to understand about DTOs and exception handling,@exceptionHandler and Validation  use @Valid anootation note: validation is generally apply on dto not entities,and also learn Pagination and use @RequestParam(value = " "ByDefault = jitna hma  rakhna hai),pageable interface that mean we cannt make object then  PageRequest,pageable ka implementation class hai  (jo pagination ka implementation provide krta hai) to isliye hm pagerequest ka object banate hain,uploading file and downloading file

--- src/main/java/com/mybasket/app/entity/User.java
+++ src/main/java/com/mybasket/app/entity/User.java
@@ -54,8 +54,8 @@ public class User extends BaseEntity {
     private Set<Address>addresses = new HashSet<>();
 
 
-@OneToMany(mappedBy = "user",fetch = FetchType.LAZY)
-private Set<PaymentMethodInfo> paymentMethodInfos = new LinkedHashSet<>();
+   @OneToMany(mappedBy = "user",fetch = FetchType.LAZY)
+   private Set<PaymentMethodInfo> paymentMethodInfos = new LinkedHashSet<>();
 
 
 }
